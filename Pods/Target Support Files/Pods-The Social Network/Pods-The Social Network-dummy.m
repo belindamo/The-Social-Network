7d9d9dfecd7812e576b083df260159001d1fbcab
Belinda <belindamomo@gmail.com>
2017-09-01 00:53:14 -0700

    set up firebase integration

---
+++ Pods/Target Support Files/Pods-The Social Network/Pods-The Social Network-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_The_Social_Network : NSObject
+@end
+@implementation PodsDummy_Pods_The_Social_Network
+@end
---
+++ The Social Network/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  The Social Network
+//
+//  Created by Belinda Mo on 9/1/17.
+//  Copyright © 2017 Belinda Mo. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
