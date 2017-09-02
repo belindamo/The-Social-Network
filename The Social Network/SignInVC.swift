feaabd26544b126cab4e8705182664f2b196f1d3
Belinda <belindamomo@gmail.com>
2017-09-02 05:46:09 -0700

    fb auth

--- The Social Network/SignInVC.swift
+++ The Social Network/SignInVC.swift
@@ -7,19 +7,50 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class SignInVC: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func FacebookBtnPressed(_ sender: Any) {
+        
+        let fbLogin = FBSDKLoginManager()
+        
+        fbLogin.logIn(withReadPermissions: ["email"], from: self, handler: {(result, error) in
+            if error != nil {
+                print("BMO: Unable to authenticate with Facebook")
+            } else if result?.isCancelled == true {
+                print("BMO: User cancelled FB authentication")
+            } else {
+                print("BMO: User successfully authenticated FB")
+            }
+            
+            let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+            self.fireBaseAuth(credential)
+        })
+    }
+    
+    func fireBaseAuth(_ credential: AuthCredential) {
+        Auth.auth().signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("BMO: Unable to authenticate with Firebase - \(error)")
+            } else {
+                print("BMO: Successfully authenticated with FB")
+            }
+        })
+    }
 
 }
 
