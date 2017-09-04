3de153fc7b6693e2a234350e70b705227cc0a0de
Belinda <belindamomo@gmail.com>
2017-09-03 18:59:16 -0700

    feed ui

--- The Social Network/CircleView.swift
+++ The Social Network/CircleView.swift
@@ -1,27 +1,32 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  The Social Network
 //
-//  Created by Belinda Mo on 9/1/17.
+//  Created by Belinda Mo on 9/2/17.
 //  Copyright © 2017 Belinda Mo. All rights reserved.
-//  
-//  custom views are essential to know for editing the
-//  look of your app.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class CircleView: UIImageView {
 
     override func awakeFromNib() {
-        super.awakeFromNib() //necessary
+        super.awakeFromNib()
         
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0 //how far it blurs out
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0) //how far the shadow goes
-        layer.cornerRadius = 2.0
         
     }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width/2
+
+    }
+    
+
+    
 
 }
